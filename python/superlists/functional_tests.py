934f6cfdbf1842053de8507f49ef9ccfa7553013
Wolfgang Sanyer <wolfgang.sanyer@gmail.com>
2014-11-20 15:39:15 -0500

    second item fail

--- python/superlists/functional_tests.py
+++ python/superlists/functional_tests.py
@@ -44,20 +44,27 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear on table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a textbox inviting her to enter another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+    
         # The page updates again, and now shows both items on her list
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+             [row.text for row in rows]
+        )
+        
         # Edith wonders whether the site will remeber her list.  Then she sees that the
         # site has generated a unique URL for her -- there is some explanatory text to
         # that effect
+        self.fail('Finish the test!')
 
         # She visits the URL - her to-do list is still there.
 
