f41c123c8059e5cf861be32963d3005ac68c890e
Wolfgang Sanyer <wolfgang.sanyer@gmail.com>
2014-11-20 13:51:44 -0500

    Front page HTML now generated from a template

--- python/superlists/functional_tests.py
+++ python/superlists/functional_tests.py
@@ -24,34 +24,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title & header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1')
-        print(header_text)
-        #self.assertIn('To-Do', header_text)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item right away
-        #inputbox = self.browser.find_element_by_id('id_new_item')
-        #self.assertEqual(
-        #        inputbox.get_attribute('placeholder'),
-        #        'Enter a to-do item'
-        #)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a textbox
         # (Edith's hobby is fly-fishing lures)
-        #inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1. Buy peacock feathers" as an item in a to-do list
-        #inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows)
-        #)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear on table"
+        )
 
         # There is still a textbox inviting her to enter another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        #self.fail('Finish the test!')
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
