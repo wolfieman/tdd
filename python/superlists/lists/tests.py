ca896ff17410bb1c212a08077d33e16fa3d9c47d
Wolfgang Sanyer <wolfgang.sanyer@gmail.com>
2014-11-21 13:18:49 -0500

    Model for list Items and associated migration

--- python/superlists/lists/tests.py
+++ python/superlists/lists/tests.py
@@ -5,6 +5,8 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 
+from lists.models import Item
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -29,3 +31,23 @@ class HomePageTest(TestCase):
             {'new_item_text':  'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        
